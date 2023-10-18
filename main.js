69ecab9047addf5388d4ff3600df9c479e95b131
Hello!!! Eu sou Fabiana Cunha!!! <107757832+ProfFabiana@users.noreply.github.com>
2023-10-18 11:27:42 -0300

    mainjs tualizado

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-alert("sou um arquivo JavaScript e fui executado!");
+function somTocaGrilo (){
+
+    document.querySelector("#som_tecla_grilo").play();
+}
+
+document.querySelector(".tecla_grilo").onclick = somTocaGrilo;
+
+const listaDeTeclas = document.querySelectorAll(".tecla");
+listaDeTeclas[4].onclick = tocaSomGrilo;
